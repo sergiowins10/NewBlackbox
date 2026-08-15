55cee0907ad7e023606a02192bc5be1bfa53f669
sergiowins10 <sergio.wins10@gmail.com>
2026-08-15 20:23:31 +0400

    Update MainActivity.kt

--- app/src/main/java/top/niunaijun/blackboxa/view/main/MainActivity.kt
+++ app/src/main/java/top/niunaijun/blackboxa/view/main/MainActivity.kt
@@ -64,7 +64,7 @@ class MainActivity : LoadingActivity() {
             initToolbarSubTitle()
 
             
-            checkStoragePermission()
+            // checkStoragePermission()
 
             
             checkVpnPermission()
